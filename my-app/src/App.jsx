3c2c0c66518b07a56553749816619d8a2cd740cb
CoolCodeMan <mmssposti94@gmail.com>
2024-06-05 23:10:17 +0300

    style update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,42 +6,32 @@ export default function App() {
         <div id="navBar">
         <h2>💻🦍 My Github Page ☀️</h2>
 
-        <br/><br/><br/>
+        <br/><br/>
 
-        <ul>
+     
 
-            <li>
+            
                 <a href="#/Infopage">Info</a>
-            </li>
+           
             <br/>
 
-
-            <li>
                 <a href="#/Blackjackgame">Blackjack Game</a>
-            </li>
-            <br/>
+            <br/><br/>
 
-            <li>
+
                 <a href="#/Texts">Texts</a>
-            </li>
-            <br/><br/><br/><br/>
-
-            <li>
+            <br/><br/>
                 <a href="#/Projects">Projects</a>
-            </li>
 
             <br/>
 
-            <li>
                 <a href="#/Updates">Site Updates</a>
-            </li>
             <br/>
             
 
 
 
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/>
 
 
 
@@ -49,13 +39,10 @@ export default function App() {
 
 
 
-            <li>
-                <a href="#/MonkeyPage">An AI made picture of a literal monkey on a computer</a>
-            </li>
+                <a href="#/MonkeyPage">???</a>
 
 
 
-        </ul>
         </div>
 
 
