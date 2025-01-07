ecd73e36e296322a119b9c87b2074048df834ca8
coolcodeman <mmssposti94@gmail.com>
2025-01-07 22:10:15 +0200

    kk

--- my-app/src/pages/Texts.jsx
+++ my-app/src/pages/Texts.jsx
@@ -8,9 +8,9 @@ export default function Texts() {
             {/* <li><a href="#/articles/ArticleTemplate">Article Name - Template</a><br/><br/></li> */}
 
              
+            <li><b><a href="#/articles/MachineLearningTools">Useful ML tools - 01/06/2025</a></b><br/><br/></li>
             <li><b><a href="#/articles/BashCommands">Useful BASH Commands - 06/06/2024</a></b><br/><br/></li>
             <li><b><a href="#/articles/SQLCommands">Useful SQL Commands - 06/06/2024</a></b></li>
-            <li><b><a href="#/articles/MachineLearningTools">Useful ML tools - 01/06/2025</a></b><br/><br/></li>
 
             </ul>
 
