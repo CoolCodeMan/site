a5d2d641fdfaf00d5b59d0cfc324d396d234880f
coolcodeman <mmssposti94@gmail.com>
2025-01-07 21:51:16 +0200

    ok

--- my-app/src/pages/articles/AITools.jsx
+++ my-app/src/pages/articles/AITools.jsx
@@ -3,17 +3,7 @@ export default function AiTools() {
         <>
         <br/>
         <a href="#/Texts"> ← Back</a>
-        <h1>AI Tools</h1>
-
-        <h3>For datamining:</h3>
-        <ul>
-            <li>Bash scripts like grep</li>
-            <li>Python scripts for extracting .csv or .tsv data from things like pdfs, html websites etc.</li>
-        </ul>
-
-        
-        <br/>
-        <p>more to be added...</p>
+        <h3>AI Tools:</h3><br/>
 
         </>
     )
