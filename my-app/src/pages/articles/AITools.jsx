92e03c9cba781740869909a9d0db5a00e35a04b9
coolcodeman <mmssposti94@gmail.com>
2025-01-07 21:40:03 +0200

    gg

--- my-app/src/pages/articles/AITools.jsx
+++ my-app/src/pages/articles/AITools.jsx
@@ -1,11 +1,19 @@
-export default function ArticleTemplate() {
+export default function AITools() {
     return(
         <>
         <br/>
         <a href="#/Texts"> ← Back</a>
         <h1>AI Tools</h1>
 
-        <p>When </p>
+        <h3>For datamining:</h3>
+        <ul>
+            <li>Bash scripts like grep</li>
+            <li>Python scripts for extracting .csv or .tsv data from things like pdfs, html websites etc.</li>
+        </ul>
+
+        
+        <br/>
+        <p>more to be added...</p>
 
         </>
     )
