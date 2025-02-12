cd98ab69f67ea72d192d0781881eefd466a3b710
coolcodeman <mmssposti94@gmail.com>
2025-02-12 22:04:25 +0200

    edit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,16 +6,16 @@ export default function App() {
         <div className="topBar">
         <h2>💻🦍 My Github Page ☀️</h2>
 
-                <a href="#/Infopage" className="topBarLink">❓ Info &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                <a href="#/Infopage" className="topBarLink">❓ Info &nbsp;&nbsp;&nbsp;&nbsp;</a>
 
-                <a href="#/Projects" className="topBarLink">👷 Projects &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                <a href="#/Projects" className="topBarLink">👷 Projects &nbsp;&nbsp;&nbsp;&nbsp;</a>
 
-                 <a href="#/Texts" className="topBarLink">📖 Articles &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                 <a href="#/Texts" className="topBarLink">📖 Articles &nbsp;&nbsp;&nbsp;&nbsp;</a>
 
-                <a href="#/Updates" className="topBarLink">🛠️ Updates &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                <a href="#/Updates" className="topBarLink">🛠️ Updates &nbsp;&nbsp;&nbsp;&nbsp;</a>
                 
         </div>
-        <br/><br/>
+
 
 
         <div className="outletContent">
