720af1fb55845b3f2d7a850fb9d03e959dfe19c1
coolcodeman <mmssposti94@gmail.com>
2025-02-12 21:08:24 +0200

    edit

--- my-app/src/pages/articles/MLTools.jsx
+++ my-app/src/pages/articles/MLTools.jsx
@@ -3,7 +3,18 @@ export default function MLTools() {
         <>
         <br/>
         <a href="#/Texts"> ← Back</a>
-        <p>Handy Machine Learning Tools: 1</p>
+        <h3>Handy Machine Learning Tools:</h3>
+
+        <h4>For datamining:</h4>
+            <ul>
+                <li>BASH</li>
+                <li>BeautifulSoup</li>
+            </ul>
+        <h4>For models:</h4>
+            <ul>
+                <li><a href="https://huggingface.com">huggingface.com</a></li>                
+            </ul>
+
         </>
     )
 }
