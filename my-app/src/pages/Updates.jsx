05eb3c2700706d186f713adcfa433e3d638b8355
coolcodeman <mmssposti94@gmail.com>
2025-02-15 19:28:16 +0200

    infopage render

--- my-app/src/pages/Updates.jsx
+++ my-app/src/pages/Updates.jsx
@@ -5,6 +5,7 @@ export default function Updates() {
             Updates:
         </h3>
 
+        <p><i>15/02/2025: Fixed the landing page rendering</i></p>
         <p><i>12/02/2025: Random updates</i></p>
         <p><i>06/06/2024: Added Texts section</i></p>
         <p><i>05/06/2024: Page deployed</i></p>
