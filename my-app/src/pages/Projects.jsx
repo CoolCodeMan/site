f7ebba96735cf2ac0e6467823a16112e6af5c8e6
coolcodeman <mmssposti94@gmail.com>
2025-02-19 04:00:44 +0200

    removed <li>s

--- my-app/src/pages/Projects.jsx
+++ my-app/src/pages/Projects.jsx
@@ -3,11 +3,11 @@ export default function Projects() {
         <>
         <h3>Projects</h3>
 
-        <a href="#/projects/Blackjackgame"><b>🃏 Javascript BlackJack </b>(browser game)</a>
+        <a href="#/projects/Blackjackgame"><b>🃏 Javascript BlackJack </b>(browser game)</a><br/>
 
-        <a href="https://github.com/OKgamegruuppi/game_project"><b>👾 A Pygame game course group project</b></a>
+        <a href="https://github.com/OKgamegruuppi/game_project"><b>👾 A Pygame game course group project</b></a><br/>
 
-        <a href="https://github.com/CoolCodeMan/RandomPythonStuff"><b>🐍 Python Projects</b></a>
+        <a href="https://github.com/CoolCodeMan/RandomPythonStuff"><b>🐍 Python Projects</b></a><br/>
         
 
         </>
