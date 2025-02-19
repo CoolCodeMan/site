cd42078145cd2a378e3782c6c1bc80154a0f2666
coolcodeman <mmssposti94@gmail.com>
2025-02-19 03:55:48 +0200

    removed <li>s

--- my-app/src/pages/Projects.jsx
+++ my-app/src/pages/Projects.jsx
@@ -1,22 +1,14 @@
 export default function Projects() {
     return(
         <>
-        <h3>Stuff</h3>
+        <h3>Projects</h3>
 
-        <ul>
+        <a href="#/projects/Blackjackgame"><b>🃏 Javascript BlackJack </b>(browser game)</a>
+
+        <a href="https://github.com/OKgamegruuppi/game_project"><b>👾 A Pygame game course group project</b></a>
+
+        <a href="https://github.com/CoolCodeMan/RandomPythonStuff"><b>🐍 Python Projects</b></a>
         
-            <li>
-                <a href="#/projects/Blackjackgame"><b>🃏 Javascript BlackJack </b>(browser game)</a>
-            </li><br/>
-            <li>
-                <a href="https://github.com/OKgamegruuppi/game_project"><b>👾 A Pygame game course group project</b></a>
-            </li><br/>
-            
-            <li>
-                <a href="https://github.com/CoolCodeMan/RandomPythonStuff"><b>🐍 Python Projects</b></a>
-            </li><br/>
-        
-        </ul>
 
         </>
     )
--- my-app/src/pages/Texts.jsx
+++ my-app/src/pages/Texts.jsx
@@ -4,19 +4,18 @@ export default function Texts() {
         <>
         <h3>Articles</h3>
 
-        <ul>
-            {/* <li><a href="#/articles/ArticleTemplate">Article Name - Template</a><br/><br/></li> */}
+
+        {/* <li><a href="#/articles/ArticleTemplate">Article Name - Template</a><br/><br/></li> */}
 
              
-            <li><b><a href="#/articles/MLTools">Useful Machine Learning Tools - 2024/06/06</a></b><br/><br/></li>
-            <li><b><a href="#/articles/BashCommands">Useful BASH Commands - 2024/06/06</a></b><br/><br/></li>
-            <li><b><a href="#/articles/SQLCommands">Useful SQL Commands - 2024/06/06</a></b></li>
+        <li><b><a href="#/articles/MLTools">Useful Machine Learning Tools - 2024/06/06</a></b><br/><br/></li>
+        <li><b><a href="#/articles/BashCommands">Useful BASH Commands - 2024/06/06</a></b><br/><br/></li>
+        <li><b><a href="#/articles/SQLCommands">Useful SQL Commands - 2024/06/06</a></b></li>
 
-            </ul>
 
-            <div>
-                <Outlet/>
-            </div>
+        <div>
+            <Outlet/>
+        </div>
             
             </>
     )
